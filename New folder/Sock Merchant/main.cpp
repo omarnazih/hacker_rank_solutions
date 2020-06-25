b2ee37b0c6c8138832d61ee19002793967e60d09
omar nazih <omarnazeh295@gmail.com>
2020-06-25 15:07:06 +0200

    Sock Marchant

---
+++ New folder/Sock Merchant/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int c = 0;
+    cin >> n ;
+    int arr[n];
+
+    for(int i = 0 ; i < n ; i++){
+            cin >> arr[i];
+    }
+
+    for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j < n ; j++){
+            if(arr[i] == arr[j])
+                c++;
+        }
+    }
+
+    cout << c;
+
+    return 0;
+}
