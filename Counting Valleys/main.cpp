a25677b23a6eada786dba010f0086472b9f596ef
omar nazih <omarnazeh295@gmail.com>
2020-06-26 14:20:08 +0200

    Counting Valleys

--- Counting Valleys/main.cpp
+++ Counting Valleys/main.cpp
@@ -4,6 +4,37 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    /*
+        Sample Input
+
+            8
+         UDDDUDUU
+
+        Sample Output
+
+            1
+                    */
+    int n;
+    cin >> n;
+    int uc =0;
+    int dc = 0;
+    char arr[n];
+
+    for(int i = 0 ; i < n ; ++i){
+        cin >> arr[i];
+    }
+
+       for(int i = 0 ; i < n ; ++i){
+             if(arr[i] == 'u'){
+            uc++;
+        }
+        else if(arr[i]=='d'){
+            dc++;
+            }
+        }
+
+    if(uc == dc){
+        cout << (uc+dc)/n << endl;
+    }
     return 0;
 }
